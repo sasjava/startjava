adb9d55cc39cd76f838ade0363c7c2408c06cd4f
ababanov.local <ababanov.local@SIGMA-05961.INT>
2023-01-23 22:04:50 +0300

    Lesson1 IfElseStatementTheme

--- Lesson_1/IfElseStatementTheme.java
+++ Lesson_1/IfElseStatementTheme.java
@@ -1,6 +1,6 @@
 public class IfElseStatementTheme {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("1.Перевод псевдокода на язык Java");
         byte age = 19;
         if (age > 20) {
@@ -40,55 +40,70 @@ public class IfElseStatementTheme {
             System.out.println("Number " + number1 + " equals " + number2);
         }
 
-        System.out.println("\n3.Проверка числа-------");
+        System.out.println("\n3.Проверка числа");
         int number3 = 124;
+        System.out.print("Number " + number3 + " is ");
         if (number3 == 0) {
-            System.out.println("Number " + number3 + " is zero");
-        } else if(number3 % 2 == 0) {
-            System.out.println("Number " + number3 + " is even");
+            System.out.println("zero");
         } else {
-            System.out.println("Number " + number3 + " is odd");
+            if(number3 % 2 == 0) {
+                System.out.print("even");
+            } else {
+                System.out.print("odd");
+            }
+            System.out.print(" and ");
+            if (number3 > 0) {
+                System.out.println("positive");
+            } else {
+                System.out.println("negative");
+            }
         }
 
         System.out.println("\n4.Поиск одинаковых цифр в числах");
-        int numberA = 389;
-        int numberB = 568;
-        boolean matchFound = false;
+        number1 = 123;
+        number2 = 124;
+        boolean isEqual = false;
 
-        System.out.println("Comparing numbers: " + numberA + " and " + numberB + " ");
-        for (int i = 0; i <= 2; i++) {
-            int digitA = numberA / (int) Math.pow(10,i) % 10;
-            int digitB = numberB / (int) Math.pow(10,i) % 10;
-            if (digitA == digitB) {
-                System.out.println("in both numbers the " + (i + 1) + " digit is " + digitA);
-                matchFound = true;
-            }
+        System.out.println("Comparing numbers: " + number1 + " and " + number2 + " ");
+        int digit1 = number1 % 10;  
+        int digit2 = number2 % 10;  
+        if (digit1 == digit2) {
+            System.out.println("in both numbers the " + 1 + " digit is " + digit1);
+            isEqual = true;
         }
-        if (!matchFound) {
+        digit1 = number1 / 10 % 10;  
+        digit2 = number2 / 10 % 10;  
+        if (digit1 == digit2) {
+            System.out.println("in both numbers the " + 2 + " digit is " + digit1);
+            isEqual = true;
+        }
+        digit1 = number1 / 10 / 10 % 10;  
+        digit2 = number2 / 10 / 10 % 10;  
+        if (digit1 == digit2) {
+            System.out.println("in both numbers the " + 3 + " digit is " + digit1);
+            isEqual = true;
+        }
+        if (!isEqual) {
             System.out.println("No matches found");
         }
 
         System.out.println("\n5.Определение символа по его коду");
         char char5 = '\u0057';
         System.out.println("Symbol: " + char5);
-        switch (char5) {
-            case 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
-                    't','u','v','w','x','y','z' ->
-                System.out.println("Symbol: " + char5);
-            case 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S',
-                    'T','U','V','W','X','Y','Z' ->
-                System.out.println("It's UpperCase Letter");
-            case '0','1','2','3','4','5','6','7','8','9' ->
-                System.out.println("It's Digit");
-            default -> System.out.println("It's unknown symbol");
+        if (char5 >= 'a' && char5 <= 'z') {
+            System.out.println("It's LowerCase Letter");
+        } else if (char5 >= 'A' && char5 <= 'Z') {
+            System.out.println("It's UpperCase Letter");
+        } else if (char5 >= '0' && char5 <= '9') {
+            System.out.println("It's Digit");
+        } else {
+            System.out.println("It's unknown symbol");
         }
 
         System.out.println("\n6.Подсчет суммы вклада и начисленных банком %");
         float deposit = 300000;
-        int percent = 0;
-        if (deposit < 100000) {
-            percent = 5;
-        } else if (deposit >= 100000 & deposit <= 300000) {
+        int percent = 5;
+        if (deposit >= 100000 & deposit <= 300000) {
             percent = 7;
         } else if (deposit > 300000) {
             percent = 10;
@@ -100,35 +115,29 @@ public class IfElseStatementTheme {
         System.out.println("Итоговая сумма " + totalSum);
 
         System.out.println("\n7.Определение оценки по предметам");
-        final String history = "История";
-        final String programming = "Программирование";
-        byte percHistory = 59;
-        byte percProgramming = 91;
-        byte gradeHistory = 0;
-        byte gradeProgramming = 0;
+        byte percentHistory = 59;
+        byte percentProgramming = 91;
+        byte gradeHistory = 2;
+        byte gradeProgramming = 2;
         System.out.println("Оценки по предметам:");
-        if (percHistory <= 60) {
-            gradeHistory = 2;
-        } else if (percHistory > 91) {
+        if (percentHistory > 91) {
             gradeHistory = 5;
-        } else if (percHistory > 73) {
+        } else if (percentHistory > 73) {
             gradeHistory = 4;
-        } else if (percHistory > 60) {
+        } else if (percentHistory > 60) {
             gradeHistory = 3;
         }
-        if (percProgramming <= 60) {
-            gradeProgramming = 2;
-        } else if (percProgramming > 91) {
+        if (percentProgramming > 91) {
             gradeProgramming = 5;
-        } else if (percProgramming > 73) {
+        } else if (percentProgramming > 73) {
             gradeProgramming = 4;
-        } else if (percProgramming > 60) {
+        } else if (percentProgramming > 60) {
             gradeProgramming = 3;
         }
-        System.out.println(gradeHistory + "\t" + history);
-        System.out.println(gradeProgramming + "\t" + programming);
+        System.out.println(gradeHistory + "\t" + "История");
+        System.out.println(gradeProgramming + "\t" + "Программирование");
         float averageGrade = (gradeHistory + gradeProgramming) / 2;
-        float averagePercent = (percHistory + percProgramming) / 2;
+        float averagePercent = (percentHistory + percentProgramming) / 2;
         System.out.println("Средний балл оценок по предметам\t" + averageGrade);
         System.out.println("Средний балл % по предметам     \t" + averagePercent);
 
@@ -138,15 +147,15 @@ public class IfElseStatementTheme {
         int monthGoodsCost = 9000;
         int yearProfit = (monthSalesIncome - monthRent - monthGoodsCost) * 12;
         if (yearProfit > 0) {
-            System.out.println("Прибыль за год: " + "+" + yearProfit);
+            System.out.println("Прибыль за год: +" + yearProfit);
         } else {
             System.out.println("Прибыль за год: " + yearProfit);
         } 
 
         System.out.println("\n9.Подсчет количества банкнот");
         int requiredSum = 567;
-        int numATM100 = 1;
-        int numATM10 = 2;
+        int numATM100 = 3;
+        int numATM10 = 4;
         int numATM1 = 460;
         int banknote100 = requiredSum / 100;
         int banknote10 = requiredSum % 100 / 10;
@@ -158,7 +167,7 @@ public class IfElseStatementTheme {
         System.out.println(banknote1 + " банкнот по 1$");
         System.out.println("\nВ банкомате имеется:\n" + numATM100 + " банкнот по 100$\n" + 
             numATM10 + " банкнот по 10$\n" + numATM1 + " банкнот по 1$");
-        System.out.println("Всего " + (numATM100 * 100 + numATM10 * 10 + numATM1));
+        System.out.println("Всего " + (numATM100 * 100 + numATM10 * 10 + numATM1) + "$");
         numATM100 -= banknote100;
         numATM10 -= banknote10;
         numATM1 -= banknote1;
