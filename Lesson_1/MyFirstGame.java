63cd2bbbeb27ab1db2978c05391450075e26b242
ababanov.local <ababanov.local@SIGMA-05961.INT>
2023-01-31 21:56:53 +0300

    Lesson1 MyFirstGame1

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,28 +1,28 @@
 public class MyFirstGame {
     
     public static void main(String[] args) {
-        int number = 5;
+        int secretNum = 5;
         int min = 1;
         int max = 100;
         int playerNumber = max / 2;
         int offset = max / 2;
         int count = 0;
-        while (number != playerNumber) {
+        while (secretNum != playerNumber) {
             count++;
             if (offset > 1) {
                 offset /= 2;
             }
             System.out.print("число " + playerNumber);
-            if (playerNumber > number) {
+            if (playerNumber > secretNum) {
                 System.out.print(" больше");
                 playerNumber -= offset;
-            } else if (playerNumber < number) {
+            } else if (playerNumber < secretNum) {
                 System.out.print(" меньше");
                 playerNumber += offset;
             }
             System.out.println(" того, что загадал компьютер");
         }
-        System.out.println("Вы победили! Компьютер загадал число " + number);
+        System.out.println("Вы победили! Компьютер загадал число " + secretNum);
         System.out.println("Вы угадали за " + count + " попытки!");
     }
 }
