93f6e496b7848389fb31014d744f0e011352d5a6
ababanov.local <ababanov.local@SIGMA-05961.INT>
2023-02-02 10:42:05 +0300

    Lesson2_01

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    char gender = 'M';
+    String name = "Aleks";
+    int height = 182;
+    int weight = 80;
+    int age = 40;
+
+    void go() {
+
+    }
+
+    void sit() {
+
+    }
+
+    void run() {
+
+    }
+
+    void speak() {
+
+    }
+
+    void learnJava() {
+        
+    }
+}
---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,26 @@
+public class Wolf {
+    char gender;
+    String nickname;
+    int weight;
+    int age;
+    String color;
+    
+    void go() {
+        System.out.println("Идёт");
+    }
+
+    void sit() {
+        System.out.println("Сидит");
+    }
+
+    void run() {
+        System.out.println("Бежит");
+    }
+    void howl() {
+        System.out.println("Воет");
+    }
+    
+    void hunt() {
+        System.out.println("Охотится");
+    }
+}
