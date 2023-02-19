c74dd2a62e1c71e94163c558d5902651819b6861
ababanov.local <albabanov@mail.ru>
2023-02-19 20:21:55 +0300

    Merge remote-tracking branch 'origin/master'

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -6,8 +6,8 @@ import java.util.Random;
 public class ArrayTheme {
     public static void main(String[] args) {
         reversArr();
-        multiplyArr();
-        deleteFromArr();
+        multiplyArrItems();
+        nullingArrayItems();
         displayStairs();
         generateUniqueNumbers();
         shiftArr();
@@ -16,79 +16,70 @@ public class ArrayTheme {
     private static void reversArr() {
         System.out.println("1. Реверс значений массива");
         int[] intArr = {1, 2, 3, 4, 5, 6, 7}; //{6, 4, 7, 1, 3, 2, 5};
-        int maxIndex = intArr.length - 1;
-        int halfLength = intArr.length / 2;
+        int len = intArr.length;
+        int maxIndex = len - 1;
         printIntArr(intArr);
-        for (int i = 0; i < halfLength; i++) {
-            intArr[i] += intArr[maxIndex - i];
-            intArr[maxIndex - i] = intArr[i] - intArr[maxIndex - i];
-            intArr[i] -= intArr[maxIndex - i];
+        for (int i = 0; i < len; i++) {
+            int intTmp = intArr[i];
+            intArr[i] = intArr[maxIndex - i];
+            intArr[maxIndex - i] = intTmp;
+            len--;
         }
         printIntArr(intArr);
         System.out.println();
     }
 
-    private static void multiplyArr() {
+    private static void multiplyArrItems() {
         System.out.println("2. Вывод произведения элементов массива");
         int[] intArr = new int[10];
-        int[] intArr2 = {0, 9};
         for (int i = 0; i < intArr.length; i++) {
             intArr[i] = i;
         }
-        String strResult = "";
-        String strResult2 = "";
-        String strTmp;
-        int intResult = 0;
+        int maxIndex = intArr.length - 1;
+        int intResult = 1;
         for (int i = 0; i < intArr.length; i++) {
-            if (existValInArr(intArr2, intArr[i])) {
-                strTmp = "[" + i + "] = " + intArr[i];
-                strResult2 = strResult2.isBlank() ? strTmp : strResult2 + ", " + strTmp;
+            if (i == 0 || i == maxIndex) {
+                if (i == maxIndex) {
+                    System.out.println(" = " + intResult);
+                }
+                System.out.println("[" + i + "] = " + intArr[i]);
             } else {
-                intResult = intResult == 0 ? intArr[i] : intResult * intArr[i];
-                strResult = strResult.isBlank() ? intArr[i] + "" : strResult + "*" + intArr[i];
+                intResult *= intArr[i];
+                if (intResult > 1) {
+                    System.out.print("*");
+                }
+                System.out.print(intArr[i]);
             }
         }
-        strResult = strResult + " = " + intResult;
-        System.out.println(strResult);
-        System.out.println(strResult2);
-        System.out.println();
     }
 
-    private static void deleteFromArr() {
+    private static void nullingArrayItems() {
         System.out.println("3. Удаление элементов массива");
         float[] floatArr = new float[15];
-        float averVal = 0;
-        int averValIndex = 0;
+        float averItemVal = 0;
+        int averIndex = floatArr.length / 2;
         float difference = 1;
         int count = 0;
         Random random = new Random();
 
         for (int i = 0; i < floatArr.length; i++) {
             floatArr[i] = random.nextFloat() ;
-            averVal += floatArr[i];
         }
-        averVal /= floatArr.length;
         System.out.println("Исходный массив:");
         printFloatArr(floatArr);
 
-        for (int i = 0; i < floatArr.length; i++) {
-            if (difference > Math.abs(floatArr[i] - averVal)) {
-                difference = Math.abs(floatArr[i] - averVal);
-                averValIndex = i;
-            }
-        }
-        System.out.printf("Среднее значение: [%s] %6.3f\n", averValIndex, floatArr[averValIndex]);
+        averItemVal = floatArr[averIndex];
+        System.out.printf("Значение среднего элемента: [%s] %6.3f\n", averIndex, averItemVal);
 
         for (int i = 0; i < floatArr.length; i++) {
-            if (floatArr[i] > averVal) {
+            if (floatArr[i] > averItemVal) {
                 floatArr[i] = 0;
                 count++;
             }
         }
         System.out.println("Новый массив:");
         printFloatArr(floatArr);
-        System.out.println("Количество обнуленных ячеек " + count);
-        System.out.println();
+        System.out.println("Количество обнуленных ячеек " + count + "\n");
     }
 
     private static void displayStairs() {
@@ -150,10 +141,7 @@ public class ArrayTheme {
                 resPosRange++;
             }
         }
-        //System.arraycopy (srcArr, 1, resArr, 0, lenResult);
-
-        System.out.println(Arrays.toString(srcArr));
-        System.out.println(Arrays.toString(resArr));
+        System.out.println(Arrays.toString(srcArr) + "\n" + Arrays.toString(resArr));
     }
     private static boolean existValInArr(int[] arr, int val) {
         for (int itemArr : arr) {
